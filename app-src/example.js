15199e10049233255f6531a17cf9f7a6eccc06a9
Diogo Paza <diogopazacvel@gmail.com>
2018-01-05 15:49:22 -0200

    workshop babel

---
+++ app-src/example.js
@@ -0,0 +1,15 @@
+console.log('fui compilado no babel');
+
+
+const trim = text => text.trim();
+
+const world = 'Calopsita do agreste'
+
+const worlds = word;
+
+
+
+
+
+
+
---
+++ app/example.js
@@ -0,0 +1,9 @@
+console.log('fui compilado no babel');
+
+var trim = function trim(text) {
+  return text.trim();
+};
+
+var world = 'Calopsita do agreste';
+var worlds = word;
+//# sourceMappingURL=example.js.map
